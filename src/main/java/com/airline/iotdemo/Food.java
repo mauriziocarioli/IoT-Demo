c54bd43d0d6e30dedd575b07eeda4cfaeb7d4399
dmAdmin <>
2019-08-20 14:37:57 -0400

    {/src/main/java/com/airline/iotdemo/Food.java}

--- src/main/java/com/airline/iotdemo/Food.java
+++ src/main/java/com/airline/iotdemo/Food.java
@@ -6,12 +6,25 @@ package com.airline.iotdemo;
 
 public class Food implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public Food() {
-    }
+	@org.kie.api.definition.type.Description(value = "Type of food.")
+	@org.kie.api.definition.type.Label(value = "Type")
+	private java.lang.String type;
 
+	public Food() {
+	}
 
+	public java.lang.String getType() {
+		return this.type;
+	}
 
+	public void setType(java.lang.String type) {
+		this.type = type;
+	}
+
+	public Food(java.lang.String type) {
+		this.type = type;
+	}
 
 }
